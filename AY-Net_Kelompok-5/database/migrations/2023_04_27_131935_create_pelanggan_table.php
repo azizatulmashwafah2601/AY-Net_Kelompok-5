3fc5dba0c6e34480f6c744cc6b7303a673b5fa3f
Adilah Qurrotu'aini <adilah.qurrotu.aini@gmail.com>
2023-04-27 21:05:20 +0700

    create tabel pelanggan & paket db

---
+++ AY-Net_Kelompok-5/database/migrations/2023_04_27_131935_create_pelanggan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePelangganTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->bigIncrements('id_pelanggan');
+            $table->string('nama_pelanggan', 30);
+            $table->string('alamat', 60);
+            $table->string('no_telp', 13);
+            $table->string('email', 30);
+            $table->string('id_paket');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pelanggan');
+    }
+}
---
+++ AY-Net_Kelompok-5/database/migrations/2023_04_27_135935_create_paket_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paket', function (Blueprint $table) {
+            $table->bigIncrements('id_paket');
+            $table->string('nama_paket', 10);
+            $table->integer('harga_paket');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paket');
+    }
+}
